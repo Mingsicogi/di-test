1bba84e80c1cf0734607c9a57fa957e449274ed8
Mingsicogi <codedoctor119@gmail.com>
2020-07-18 20:44:49 +0900

    + 연관관계 매핑

--- src/main/java/mins/study/ditest/controller/ProfessorController.java
+++ src/main/java/mins/study/ditest/controller/ProfessorController.java
@@ -1,12 +1,13 @@
 package mins.study.ditest.controller;
 
 import lombok.RequiredArgsConstructor;
-import mins.study.ditest.entity.Course;
 import mins.study.ditest.entity.Professor;
-import mins.study.ditest.repository.CourseRepository;
 import mins.study.ditest.repository.ProfessorRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/professor")
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class ProfessorController {
 
     private final ProfessorRepository professorRepository;
-    private final CourseRepository courseRepository;
 
     @PostMapping
     public ResponseEntity<Object> add(String name, Integer age) {
@@ -24,15 +24,5 @@ public class ProfessorController {
     @GetMapping
     public ResponseEntity<Object> get(Long professorId) {
         return ResponseEntity.ok(professorRepository.findById(professorId).orElseThrow());
-    }
-
-    @PutMapping
-    public ResponseEntity<Object> put(Long professorId, Long courseId) {
-        Professor professor = professorRepository.findById(professorId).orElseThrow();
-        Course course = courseRepository.findById(courseId).orElseThrow();
-
-        professor.setCourse(course);
-
-        return ResponseEntity.ok(professorRepository.save(professor));
     }
 }
